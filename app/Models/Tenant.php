e238b685b4faf6152986fe7df4772c0d0d71d0c4
txrangel <joaovitorrtd@gmail.com>
2025-03-10 18:23:39 -0300

    refactor: ajustando modelos e migrations

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Hash;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Stancl\Tenancy\Contracts\TenantWithDatabase;
 use Stancl\Tenancy\Database\Concerns\HasDatabase;
@@ -10,4 +12,26 @@ use Stancl\Tenancy\Database\Concerns\HasDomains;
 class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasDatabase, HasDomains;
+
+    public function domains(): HasMany{
+        return $this->hasMany(related: Domain::class);
+    }
+
+    public static function getCustomColumns(): array{
+        return [
+            'id',
+            'name',
+            'email',
+            'password'
+        ];
+    }
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    public function setPasswordAttribute($value): string{
+        return $this->attributes['password'] = Hash::make(value: $value);
+    }
 }
--- database/migrations/2019_09_15_000010_create_tenants_table.php
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -17,9 +17,9 @@ class CreateTenantsTable extends Migration
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->string('id')->primary();
-
-            // your custom columns may go here
-
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
             $table->json('data')->nullable();
         });
